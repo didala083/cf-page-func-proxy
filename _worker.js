00597357b087f3830ec760b10de1807e9a718576
didala083 <64563710+didala083@users.noreply.github.com>
2022-10-13 08:29:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '34.94.248.116'
+      url.hostname = '34.94.248.116:80'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
