dfdec09b93c970eebcc98521177f885abd31d019
didala083 <64563710+didala083@users.noreply.github.com>
2022-10-13 08:38:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '34.94.248.116:80'
+      url.hostname = '2cywebs.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
