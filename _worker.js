1c6a0a6d71f6c2f4df221f114fd9ae8950335cf8
didala083 <64563710+didala083@users.noreply.github.com>
2022-10-14 09:54:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '2cywebs.com'
+      url.hostname = 'https://yjj2s-my.sharepoint.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
